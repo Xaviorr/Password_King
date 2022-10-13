2a46f7627d021e98960543216ad25b26e021d073
Xaviorr <franzz@charter.net>
2022-10-12 21:41:53 -0400

    Organized code and fixed Search feature

--- app/src/main/java/com/onemorelvl/passwordking/AccountDao.java
+++ app/src/main/java/com/onemorelvl/passwordking/AccountDao.java
@@ -7,20 +7,22 @@ import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
+
 import java.util.List;
+
 import io.reactivex.rxjava3.core.Completable;
 
 @Dao
 public interface AccountDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    public void insert(PasswordKingModel account);
+    void insert(PasswordKingModel account);
 
     @Update
-    public Completable update(PasswordKingModel account);
+    Completable update(PasswordKingModel account);
 
     @Delete
-    public Completable delete(PasswordKingModel account);
+    Completable delete(PasswordKingModel account);
 
     @Query("SELECT * FROM account_table ORDER BY id ASC")
     LiveData<List<PasswordKingModel>> getAllAccounts();
